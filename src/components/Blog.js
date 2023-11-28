1fb0d6398b83b7c2418f9bd09a79d4b4b96ac101
EvanWilson9 <wilsoeva002@warren.k12.in.us>
2023-11-28 10:57:35 -0500

    work on dialog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,14 +5,42 @@ import CreatePostModule from './CreatePostModule';
 
 function Blog(){
 
-    document.querySelector('.create-post')
+    const CreatePostModule = ()=>{
+       
+        return(
+            <>
+               
+            </>
+        )
+    }
+
+    const dialog = document.getElementById('dialog');  
 
     return(
         <>
             <TopHeader/>
                 <div className="blog-container">
                 <BlogPosts/>
-                <button className="create-post">Create Post</button>
+                <dialog closed id="dialog">
+                    <form id="dialog-form" method="dialog">
+                        <div>Create Post</div>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="image"/>
+                        <button>Create</button>
+                        <div className='exit-dialog' onClick={()=>{
+                            dialog.style.display="none";
+                        }}>
+                            X
+                        </div>
+                    </form>
+                </dialog>
+                <button onClick={()=>{
+                    dialog.style.display="block";  
+                }} className="create-post">
+                    Create Post
+                </button>
+                
             </div>
         </>
     )
