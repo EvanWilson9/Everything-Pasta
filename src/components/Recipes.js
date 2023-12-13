90e8e51af661e74bbe836e5c8b3e5f3856a67f87
EvanWilson9 <wilsoeva002@warren.k12.in.us>
2023-12-13 13:25:04 -0500

    commit

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,19 +1,69 @@
 import React from "react";
 import ContactForm from "./ContactForm";
 
-function Recipes(){
-    return(
+function Recipes() {
+
+    const Dish = ()=>{
+        return(
+            <div className="dish">
+                <img className="dish-img" src='/images/coming-soon.jpg'/>
+                <div className="dish-name">Dish name</div>
+                <div className="dish-desc">Dish description</div>
+            </div>
+        )
+    }
+
+
+    const searchBar = document.querySelector('.recipes-search');
+    const clearSearchBtn = document.querySelector('.clear-search');
+
+    const clearSearch = () => {
+        searchBar.value = '';
+    }
+
+    const showClear = () => {
+        if (searchBar.value !== '') {
+            clearSearchBtn.style.display = 'block';
+        }
+    }
+
+    const hideClear = () => {
+
+        const timeoutAction = () => {
+            clearSearchBtn.style.display = 'none';
+        }
+
+        setTimeout(timeoutAction, 100);
+    }
+
+    return (
         <>
             <div className="blog-container">
                 <div className='blog-section-title-wrapper'>
                     <div className='blog-section-title'>
                         Explore our pasta catalog!
-                        </div>
                     </div>
-                    <div className='posts-container'></div>
+                </div >
+                <div className="search-section">
+                    <div className="search-container">
+                        <input
+                            onFocus={showClear}
+                            onChange={showClear}
+                            onBlur={hideClear}
+                            type='text' placeholder="Search" className="recipes-search" />
+                        <div className="clear-search" onClick={clearSearch}>X</div>
+                    </div>
                 </div>
-                <ContactForm/>  
-        </>  
+                <section className="recipes-section">
+                    <Dish/>
+                    <Dish/>
+                    <Dish/>
+                    <Dish/>
+                    <Dish/>
+                </section>
+            </div>
+            <ContactForm />
+        </>
     )
 }
 
